5cde7ac813d36671cf7106e54a405c95bef1c1e6
Alec <2013056093@ust.edu.ph>
2016-12-12 01:05:33 +0800

    fixed search functionality

--- app/src/main/java/com/example/jonas/pocketaid/Adapters/InjuryListAdapter.java
+++ app/src/main/java/com/example/jonas/pocketaid/Adapters/InjuryListAdapter.java
@@ -1,43 +1,108 @@
 package com.example.jonas.pocketaid.Adapters;
 
-import android.app.Activity;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.jonas.pocketaid.R;
 
+import java.util.ArrayList;
+
 /**
  * Created by alec on 12/10/16.
  */
-public class InjuryListAdapter extends ArrayAdapter<String> {
+public class InjuryListAdapter extends BaseAdapter implements Filterable {
 
-    private final Activity context;
-    private final String[] injuryName;
-    private final Integer[] imageId;
+    Context context;
+    ArrayList<Injury> injuries;
+    FilterClass filterClass;
+    ArrayList<Injury> filteredInjuries;
 
-    public InjuryListAdapter(Activity context, String[] injuryName, Integer[] imageId) {
-        super(context, R.layout.layout_injury_list, injuryName);
-
+    public InjuryListAdapter(Context context, ArrayList<Injury> injuries) {
         this.context = context;
-        this.injuryName = injuryName;
-        this.imageId = imageId;
+        this.injuries = injuries;
+        this.filteredInjuries = injuries;
     }
 
-    public View getView(int position, View view, ViewGroup parent) {
-        LayoutInflater inflater = context.getLayoutInflater();
-        View rowView = inflater.inflate(R.layout.layout_injury_list, null, true);
+    @Override
+    public int getCount() {
+        return injuries.size();
+    }
 
-        TextView injuryView = (TextView) rowView.findViewById(R.id.injury_name);
-        ImageView imageView = (ImageView) rowView.findViewById(R.id.injury_icon);
+    @Override
+    public Object getItem(int position) {
+        return injuries.get(position);
+    }
 
-        injuryView.setText(injuryName[position]);
-        imageView.setImageResource(imageId[position]);
+    @Override
+    public long getItemId(int position) {
+        return injuries.indexOf(getItem(position));
+    }
 
-        return rowView;
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if(convertView == null) {
+            convertView = inflater.inflate(R.layout.layout_injury_list, null);
+        }
+
+        TextView injuryTextView = (TextView) convertView.findViewById(R.id.injury_name);
+        ImageView injuryImageView = (ImageView) convertView.findViewById(R.id.injury_icon);
+
+        injuryTextView.setText(injuries.get(position).getInjuryName());
+        injuryImageView.setImageResource(injuries.get(position).getInjuryLogo());
+
+        return convertView;
+    }
+
+    @Override
+    public Filter getFilter() {
+        if(filterClass == null) {
+            filterClass = new FilterClass();
+        }
+
+        return filterClass;
+    }
+
+    public class FilterClass extends Filter {
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults filterResults = new FilterResults();
+
+            if(constraint != null && constraint.length() > 0) {
+                constraint = constraint.toString().toLowerCase();
+                ArrayList<Injury> filters = new ArrayList<Injury>();
+
+                for(int ctr = 0; ctr < filteredInjuries.size(); ctr++) {
+                    if(filteredInjuries.get(ctr).getInjuryName().toLowerCase().contains(constraint)) {
+                        Injury injury = new Injury(filteredInjuries.get(ctr).getInjuryName(), filteredInjuries.get(ctr).getInjuryLogo());
+
+                        filters.add(injury);
+                    }
+                }
+
+                filterResults.count = filters.size();
+                filterResults.values = filters;
+            } else {
+                filterResults.count = filteredInjuries.size();
+                filterResults.values = filteredInjuries;
+            }
+
+            return filterResults;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            injuries = (ArrayList<Injury>) results.values;
+            notifyDataSetChanged();
+        }
     }
 }
--- app/src/main/java/com/example/jonas/pocketaid/Fragments/InjuriesFragment.java
+++ app/src/main/java/com/example/jonas/pocketaid/Fragments/InjuriesFragment.java
@@ -3,21 +3,24 @@ package com.example.jonas.pocketaid.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
+import android.widget.AdapterView;
 import android.widget.ListView;
-
+import com.example.jonas.pocketaid.Adapters.Injury;
 import com.example.jonas.pocketaid.Adapters.InjuryListAdapter;
+import com.example.jonas.pocketaid.InjuriesFragments.BitesFragment;
+import com.example.jonas.pocketaid.InjuriesFragments.BurnsFragment;
+import com.example.jonas.pocketaid.InjuriesFragments.ConcussionFragment;
+import com.example.jonas.pocketaid.InjuriesFragments.ContusionFragment;
+import com.example.jonas.pocketaid.InjuriesFragments.FractureFragment;
+import com.example.jonas.pocketaid.InjuriesFragments.LacerationFragment;
 import com.example.jonas.pocketaid.MainActivity;
 import com.example.jonas.pocketaid.R;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 
 
 /**
@@ -26,15 +29,33 @@ import java.util.HashMap;
 public class InjuriesFragment extends Fragment {
 
     private ListView listView;
-    ArrayAdapter<String> adapter;
-    EditText searchEditText;
-    ArrayList<HashMap<String, String>> injuryList;
+    private SearchView searchView;
+    InjuryListAdapter listAdapter;
+
+    //list of injuries
+    String[] injuries = {
+            "Fracture",
+            "Burns",
+            "Bites",
+            "Concussion",
+            "Contusion",
+            "Laceration"
+    };
+
+    //icons
+    Integer[] icon = {
+            R.drawable.ic_menu_camera,
+            R.drawable.ic_menu_gallery,
+            R.drawable.ic_menu_manage,
+            R.drawable.ic_menu_send,
+            R.drawable.ic_menu_share,
+            R.drawable.ic_menu_slideshow
+    };
 
 
     public InjuriesFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -43,50 +64,96 @@ public class InjuriesFragment extends Fragment {
         ((MainActivity)getActivity()).setActionBarTitle("Injuries");
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_injuries, container, false);
 
-        //list of injuries
-        String injuries[] = {
-                getResources().getString(R.string.fracture),
-                getResources().getString(R.string.burns),
-                getResources().getString(R.string.bites),
-                getResources().getString(R.string.concussion),
-                getResources().getString(R.string.contusion),
-                getResources().getString(R.string.laceration)
-        };
-
-        Integer[] imageId = {
-                R.drawable.ic_menu_camera,
-                R.drawable.ic_menu_gallery,
-                R.drawable.ic_menu_manage,
-                R.drawable.ic_menu_send,
-                R.drawable.ic_menu_share,
-                R.drawable.ic_menu_slideshow
-        };
-
         listView = (ListView) rootView.findViewById(R.id.injuries_listview);
-        searchEditText = (EditText) rootView.findViewById(R.id.injuries_search);
+        searchView = (SearchView) rootView.findViewById(R.id.injuries_search);
 
-        //for searching injuries
-        searchEditText.addTextChangedListener(new TextWatcher() {
+        listAdapter = new InjuryListAdapter(getActivity(), getInjuries());
+        listView.setAdapter(listAdapter);
+
+        //searching for an injury
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
+            public boolean onQueryTextSubmit(String query) {
+                return false;
             }
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                adapter.getFilter().filter(s);
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
+            public boolean onQueryTextChange(String newText) {
+                listAdapter.getFilter().filter(newText);
+                return false;
             }
         });
 
-        adapter = new InjuryListAdapter(getActivity(), injuries, imageId);
-        listView.setAdapter(adapter);
+        //clicking an item on list
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0) {
+                    FractureFragment fractureFragment = new FractureFragment();
+                    FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.add(fractureFragment, "Fracture")
+                            .replace(R.id.fragment_container, fractureFragment)
+                            .addToBackStack("Fracture")
+                            .commit();
+                } else if(position == 1) {
+                    BurnsFragment burnsFragment = new BurnsFragment();
+                    FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.add(burnsFragment, "Burns")
+                            .replace(R.id.fragment_container, burnsFragment)
+                            .addToBackStack("Burns")
+                            .commit();
+                } else if(position == 2) {
+                    BitesFragment bitesFragment = new BitesFragment();
+                    FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.add(bitesFragment, "Bites")
+                            .replace(R.id.fragment_container, bitesFragment)
+                            .addToBackStack("Bites")
+                            .commit();
+                } else if(position == 3) {
+                    ConcussionFragment concussionFragment = new ConcussionFragment();
+                    FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.add(concussionFragment, "Concussion")
+                            .replace(R.id.fragment_container, concussionFragment)
+                            .addToBackStack("Concussion")
+                            .commit();
+                } else if(position == 4) {
+                    BurnsFragment burnsFragment = new BurnsFragment();
+                    FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.add(burnsFragment, "Burns")
+                            .replace(R.id.fragment_container, burnsFragment)
+                            .addToBackStack("Burns")
+                            .commit();
+                } else if(position == 5) {
+                    ContusionFragment contusionFragment = new ContusionFragment();
+                    FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.add(contusionFragment, "Contusion")
+                            .replace(R.id.fragment_container, contusionFragment)
+                            .addToBackStack("Contusion")
+                            .commit();
+                } else if(position == 6) {
+                    LacerationFragment lacerationFragment = new LacerationFragment();
+                    FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.add(lacerationFragment, "Laceration")
+                            .replace(R.id.fragment_container, lacerationFragment)
+                            .addToBackStack("Laceration")
+                            .commit();
+                }
+            }
+        });
 
         return rootView;
+    }
+
+    private ArrayList<Injury> getInjuries() {
+        ArrayList<Injury> injuriesAL = new ArrayList<Injury>();
+        Injury injury;
+
+        for(int ctr = 0; ctr < injuries.length; ctr++) {
+            injury = new Injury(injuries[ctr], icon[ctr]);
+            injuriesAL.add(injury);
+        }
+
+        return injuriesAL;
     }
 
 }
