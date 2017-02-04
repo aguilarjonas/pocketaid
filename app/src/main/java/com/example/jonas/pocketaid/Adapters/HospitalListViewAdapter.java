da718c7df8ac97db0e997e602dc51a79305dc694
Lance <Lance>
2017-02-04 13:07:42 +0800

    check nearby

---
+++ app/src/main/java/com/example/jonas/pocketaid/Adapters/HospitalListViewAdapter.java
@@ -0,0 +1,15 @@
+package com.example.jonas.pocketaid.Adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.jonas.pocketaid.R;
+
+/**
+ * Created by Lance Morales on 1/31/2017.
+ */
+
+
+}
