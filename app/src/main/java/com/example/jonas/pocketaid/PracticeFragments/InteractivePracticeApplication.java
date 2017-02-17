3b503669fdccf9bd7758deb9875f158cc6d52825
raeven15 <raeven_bauto15@yahoo.com>
2017-02-17 19:52:54 +0800

    Interactive Application v1.0

--- app/src/main/java/com/example/jonas/pocketaid/PracticeFragments/InteractivePracticeApplication.java
+++ app/src/main/java/com/example/jonas/pocketaid/PracticeFragments/InteractivePracticeApplication.java
@@ -7,10 +7,19 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -18,10 +27,13 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.jonas.pocketaid.MainActivity;
 import com.example.jonas.pocketaid.R;
+
+import org.w3c.dom.Text;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -30,9 +42,21 @@ public class InteractivePracticeApplication extends Fragment {
 
     ViewGroup rootView;
     ImageView applicationImage;
-    Button btTrigger;
+    TextView tvTrigger;
     Paint paint;
     RelativeLayout relativeLayout_Touch;
+
+    ImageView material1;
+    ImageView material2;
+    ImageView material3;
+
+    boolean hasClickTrigger = false;
+    int whichMaterial = 0;
+    boolean changeImage = false;
+    boolean isItDone = false;
+    boolean nextStep = false;
+
+    int imageNumber = 1;
     public InteractivePracticeApplication() {
         // Required empty public constructor
     }
@@ -47,48 +71,141 @@ public class InteractivePracticeApplication extends Fragment {
         ((MainActivity) getActivity()).resetActionBar(true, DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
         ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        btTrigger = (Button) rootView.findViewById(R.id.triggerButton);
+
         relativeLayout_Touch = (RelativeLayout) rootView.findViewById(R.id.relativeLayout_Touch);
         relativeLayout_Touch.addView(new MyView(getActivity().getApplicationContext()));
-        addTouchListener();
+
+        tvTrigger = (TextView) rootView.findViewById(R.id.triggerTextview);
+
+        relativeLayout_Touch = (RelativeLayout) rootView.findViewById(R.id.relativeLayout_Touch);
+        relativeLayout_Touch.addView(new MyView(getActivity().getApplicationContext()));
+
+        material1 = (ImageView) rootView.findViewById(R.id.iv_application_material1);
+        material2 = (ImageView) rootView.findViewById(R.id.iv_application_material2);
+        material3 = (ImageView) rootView.findViewById(R.id.iv_application_material3);
+
+
         getTrigger();
 
 
         return rootView;
     }
 
-    private void addTouchListener() {
-//        applicationImage = (ImageView) rootView.findViewById(R.id.touchImage);
-        relativeLayout_Touch.setOnTouchListener(new View.OnTouchListener() {
-
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                float x = event.getRawX();
-                float y = event.getRawY();
-
-
-
-
-                return false;
-            }
-        });
-    }
     public void getTrigger(){
 
-        btTrigger.setOnTouchListener(new View.OnTouchListener(){
+        tvTrigger.setOnTouchListener(new View.OnTouchListener(){
+            private Rect rect;
+
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                Toast.makeText(getActivity().getApplicationContext(), "PININDOT MO YUNG SUGAT", Toast.LENGTH_SHORT).show();
+
+                switch(event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        Toast.makeText(getActivity().getApplicationContext(), "PININDOT MO YUNG SUGAT", Toast.LENGTH_SHORT).show();
+                        Log.e("SUGAT:", "ANDITO AKO SA SUGAT");
+                        hasClickTrigger = true;
+
+                    case MotionEvent.ACTION_UP:
+                        if (whichMaterial == 1 && nextStep == true){
+                            Log.e("TAMA KA:", "MAY TAMA KA ... SA ULO");
+//                            whichMaterial = 2;
+                            changeImage = true;
+                        }
+
+                        else if (whichMaterial == 2 && nextStep == true){
+                            changeImage = true;
+                        }
+
+                        else if (whichMaterial == 3 && nextStep == true){
+                            changeImage = true;
+                            isItDone = true;
+
+                        }
+
+                        else changeImage = false;
+                }
+
+
+
                 return false;
             }
         });
+
+        material1.setOnTouchListener(new View.OnTouchListener(){
+            private Rect rect;
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                //By default, the whichMaterial == 0.
+                /*
+                    This prevents the user from triggering the material again, if the user has completed
+                    the first step
+                 */
+                if (whichMaterial == 0){
+                    switch(event.getAction()){
+                        case MotionEvent.ACTION_DOWN:
+                            Toast.makeText(getActivity().getApplicationContext(), "PININDOT MO YUNG GAMIT", Toast.LENGTH_SHORT).show();
+                            whichMaterial = 1;
+                            nextStep = true;
+                            Log.e("GAMIT:", "ANDITO AKO SA GAMIT");
+                    }
+                }
+                return false;
+            }
+        });
+
+        material2.setOnTouchListener(new View.OnTouchListener(){
+            private Rect rect;
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                //By default, the whichMaterial == 0.
+                /*
+                    This prevents the user from triggering the material again, if the user has completed
+                    the second step
+                 */
+                if (whichMaterial == 1){
+                    switch(event.getAction()){
+                        case MotionEvent.ACTION_DOWN:
+                            Toast.makeText(getActivity().getApplicationContext(), "PININDOT MO YUNG GAMIT", Toast.LENGTH_SHORT).show();
+                            whichMaterial = 2;
+                            nextStep = true;
+                            Log.e("GAMIT:", "ANDITO AKO SA GAMIT");
+                    }
+                }
+                return false;
+            }
+        });
+
+        material3.setOnTouchListener(new View.OnTouchListener(){
+            private Rect rect;
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                //By default, the whichMaterial == 0.
+                /*
+                    This prevents the user from triggering the material again, if the user has completed
+                    the second step
+                 */
+                if (whichMaterial == 2){
+                    switch(event.getAction()){
+                        case MotionEvent.ACTION_DOWN:
+                            Toast.makeText(getActivity().getApplicationContext(), "PININDOT MO YUNG GAMIT", Toast.LENGTH_SHORT).show();
+                            whichMaterial = 3;
+                            nextStep = true;
+                            Log.e("GAMIT:", "ANDITO AKO SA GAMIT");
+                    }
+                }
+                return false;
+            }
+        });
+
+
 
     }
 
 
     class MyView extends View{
-
-
         Paint paint = new Paint();
         Point point = new Point();
 
@@ -101,7 +218,30 @@ public class InteractivePracticeApplication extends Fragment {
 
         @Override
         protected void onDraw(Canvas canvas) {
-            Bitmap b= BitmapFactory.decodeResource(getResources(), R.drawable.abrasion_1);
+            Log.e("onDraw:", "onDraw");
+            //Change this default to a transparent background
+            Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.ic_chemical);
+            if (imageNumber == 1){
+                 b= BitmapFactory.decodeResource(getResources(), R.drawable.abrasion_1);
+            }
+
+            else if (imageNumber == 2){
+                 b= BitmapFactory.decodeResource(getResources(), R.drawable.checkmark);
+            }
+
+            else if (imageNumber == 3){
+                b= BitmapFactory.decodeResource(getResources(), R.drawable.ic_nearby);
+            }
+
+            else if (imageNumber == 99){
+                DisplayScoreFragment displayScoreFragment = new DisplayScoreFragment();
+                FragmentTransaction fragmentTransaction = getFragmentManager ().beginTransaction();
+                fragmentTransaction.add(displayScoreFragment, "displayScoreFragment")
+                        .replace(R.id.fragment_container, displayScoreFragment)
+                        .addToBackStack("displayScoreFragment")
+                        .commit();
+            }
+
             canvas.drawBitmap(b, 0, 0, paint);
             canvas.drawCircle(point.x, point.y, 100, paint);
         }
@@ -110,16 +250,30 @@ public class InteractivePracticeApplication extends Fragment {
         public boolean onTouchEvent(MotionEvent event) {
             switch (event.getAction()) {
                 case MotionEvent.ACTION_DOWN:
-                    point.x = event.getX();
-                    point.y = event.getY();
-                    String message = String.format("Coordinates: %.2f, %.2f", point.x, point.y);
+                    point.x = event.getX() ;
+                    point.y = event.getY() ;
+                    Toast.makeText(getActivity().getApplicationContext(), "PININDOT MO YUNG PICTURE", Toast.LENGTH_SHORT).show();
+                    Log.e("PICTURE:", "ANDITO AKO SA PICTURE");
 
-                    int[] test = new int[2];
-                    btTrigger.getLocationOnScreen(test);
+                    if (hasClickTrigger == true && changeImage == true){
+                        Log.e("PICTURE:", "HEHEHE and galing ko");
+                        if (isItDone == true){
+                            imageNumber = 99;
+                            Log.e("IMAGE NUMBER:", "99");
 
-                    String message2 = "X: " + test[0] + "," + "Y: " + test[1];
-                    Log.e("COORDINATE", message);
-                    Log.e("BUTTON COORDINATE", message2);
+                        }
+
+                        else {
+                            hasClickTrigger = false;
+                            changeImage = false;
+                            imageNumber ++;
+                        }
+
+                    }
+
+                    else
+                        Log.e("PICTURE:", "HEHEHE di ka magaling");
+
 
             }
             invalidate();
